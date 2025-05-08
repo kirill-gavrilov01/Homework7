cc9fa769d74b5561252a55b52f7bc79e06771c24
homework <kirill_gavrilov01@bk.ru>
2025-05-08 16:18:59 +1000

    homework 7

--- src/Main.java
+++ src/Main.java
@@ -18,15 +18,15 @@ public class Main {
             System.out.println(" Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 0 && clientDeviceYear >=2015) {
             System.out.println(" Установите приложение для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println(" Установите облегченную версию приложения для Android по ссылке ");
-    } else {
+        } else {
             System.out.println(" Установите приложение для Android по ссылке");
         }
         System.out.println(" Задача№3");
     int year = 2021;
 
-        if (year > 1584 && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))) {
+        if (year < 1584 && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))) {
         System.out.println(year + " год является високосным");
     } else if (year > 1584) {
         System.out.println(year + " год не является високосным");
@@ -46,7 +46,7 @@ public class Main {
     } else {
             days = -1;
         }
-        System.out.println("Потребуется дней: " + days);
+        System.out.println("срок доставки " + days + " дня ");
 
         System.out.println(" Задача№5");
     int monthNumber = 12;
